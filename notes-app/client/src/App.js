bc020332f21b4f38b3ead62ace923554077f6b96
Alexander Barbaran <abarba02@gmail.com>
2019-08-20 12:38:46 -0400

    login loginforms signupforms signup

--- notes-app/client/src/App.js
+++ notes-app/client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Main from './components/Main'
-
+import Main from './main'
 import './App.css';
-
+​
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <h1>Aloha World</h1>
+      <Main />
     </div>
   );
 }
-
+​
 export default App;
---
+++ notes-app/client/src/components/HomeLoggedIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+​
+function HomeLoggedIn() {
+    return (
+        <div>
+            <h1>HomeLoggedIn</h1>
+        </div>
+    )
+}
+​
+export default HomeLoggedIn
